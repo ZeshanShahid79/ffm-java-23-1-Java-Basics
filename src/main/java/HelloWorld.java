fdc75738adfcb6dd90d268396dcf2625fd3e7652
Zeshan Shahid <z.shahid@hotmail.de>
2023-08-21 17:36:49 +0200

    Initial commit

---
+++ src/main/java/HelloWorld.java
@@ -0,0 +1,23 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Java ist cool");
+
+        int a = 12;
+        int b = 23;
+
+        int sum = a + b;
+        System.out.println(sum);
+
+        int sumMultiplication = a * b;
+        int sumAddition = a + b;
+        int sumDivision = a / b;
+        int sumSubtraction = a - b;
+
+        System.out.println(sumMultiplication);
+        System.out.println(sumAddition);
+        System.out.println(sumDivision);
+        System.out.println(sumSubtraction);
+
+
+    }
+}
